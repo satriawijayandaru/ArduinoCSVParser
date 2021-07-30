46b15793b2e656bf6710acef91beb6c3e7031771
vrey11 <yanda1998@gmail.com>
2021-07-30 22:58:36 +0700

    multiple data types working

--- wokring_teensy/wokring_teensy.ino
+++ wokring_teensy/wokring_teensy.ino
@@ -1,6 +1,5 @@
 #define hwserial Serial2
 
-
 String incomingString;
 String incomingCSV[100];
 int currentCSVIndex = 0;
@@ -14,19 +13,22 @@ void setup() {
 
 void loop() {
   if (hwserial.available() > 0) {
-    serialIN(hwserial.read(), 1);
+    serialIN(hwserial.read(), 0);
   }
+  Serial.print("Seconds from serial = ");
+  double time = (incomingCSV[1].toInt());
+  Serial.println(time/1000);
 }
 
 
 void serialIN(char incomingSerial, bool debugMode) {
 
-/*  ADD THIS VARIABLES
-  String incomingString;
-  String incomingCSV[100];
-  int currentCSVIndex = 0;
-  int maxCSVIndex;
-*/
+  /*  ADD THIS VARIABLES
+    String incomingString;
+    String incomingCSV[100];
+    int currentCSVIndex = 0;
+    int maxCSVIndex;
+  */
 
   char incomingByte;
   incomingByte = incomingSerial;
@@ -61,39 +63,3 @@ void serialIN(char incomingSerial, bool debugMode) {
     currentCSVIndex = 0;
   }
 }
-
-
-//void serialIN(char incomingSerial, bool debugMode) {
-//  char incomingByte;
-//  incomingByte = incomingSerial;
-//  incomingString += incomingByte;
-//  if (incomingByte == ',') {
-//    int str_len = incomingString.length() - 1;
-//    incomingCSV[currentCSVIndex] = incomingString.substring(0, str_len);
-//    if (debugMode == 1) {
-//      Serial.print("Incoming ");
-//      Serial.print(currentCSVIndex);
-//      Serial.print(" = ");
-//      Serial.println(incomingCSV[currentCSVIndex]);
-//    }
-//    currentCSVIndex++;
-//    incomingString = "";
-//  }
-//  if (incomingByte == '`') {
-//    int str_len = incomingString.length() - 1;
-//    incomingCSV[currentCSVIndex] = incomingString.substring(0, str_len);
-//    if (debugMode == 1) {
-//      Serial.print("Incoming ");
-//      Serial.print(currentCSVIndex);
-//      Serial.print(" = ");
-//      Serial.println(incomingCSV[currentCSVIndex]);
-//
-//      Serial.print("max Index   = ");
-//      Serial.println(maxCSVIndex);
-//      Serial.println();
-//    }
-//    incomingString = "";
-//    maxCSVIndex = currentCSVIndex;
-//    currentCSVIndex = 0;
-//  }
-//}
